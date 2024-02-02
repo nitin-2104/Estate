85c890393d139ec02a06093591e9ed98edb2a2f6
Nitin Kumar Singh <nitinkumars607@gmail.com>
2024-02-02 17:38:13 +0530

    gsap in contact

--- js/script.js
+++ js/script.js
@@ -32,3 +32,38 @@ ScrollTrigger.refresh();
 }
 
 LocomotiveAnimation();
+
+
+// function cursorAnimation() {
+
+//   document.addEventListener("mousemove", function(dets){
+//     gsap.to("#cursor", {
+//         left:dets.x,
+//         top:dets.y
+//     })
+// })
+
+// document.querySelector("#main").addEventListener("mouseenter", function(){
+// gsap.to("#cursor",{
+//     transform: 'translate(-50%,-50%) scale(1)'
+// })
+// }) 
+
+
+//     document.querySelectorAll(".child").forEach(function (element) {
+//         element.addEventListener("mouseenter", function(){
+//             gsap.to("#cursor",{
+//             transform: "translate(-50%,-50%) scale(1)"
+//             });
+//         });
+//         element.addEventListener("mouseleave", function(){
+//             gsap.to("#cursor",{
+//             transform: "translate(-50%,-50%) scale(0)"
+//             });
+//         });    
+//     });
+
+// }
+
+
+// cursorAnimation();
